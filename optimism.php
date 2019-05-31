9a1b69da5db89c3ea1832743d87368aabb17af49
danilo-cecilia <danilo.cecilia@zenabis.com>
2019-05-31 10:35:10 -0700

    Included header on a separated file

--- optimism.php
+++ optimism.php
@@ -27,26 +27,7 @@ $optimism_rows = mysqli_num_rows($optimism_result);
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#MainNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="MainNav">
-                <a href="#" class="navbar-brand">Optimism Test </a>
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Tests</a>
-                        <div class="dropdown-menu">
-                            <a href="bigFive.php" class="dropdown-item">Test 1</a>
-                            <a href="personality.php" class="dropdown-item">Test 2</a>
-                            <a href="optimism.php" class="dropdown-item">Test 3</a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php include 'header.php';?>
 
     <div class="container">
         <div id="quiz">
